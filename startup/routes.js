a9c88672b6eddf82abc50b1414bffb6b2d0d0256
GustavoFavorito <gustavofavorito.d@gmail.com>
2024-03-21 11:53:11 -0300

    adicionando cors ao header

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const indexRoute = require('../routes/index');
 const dataRoute = require('../routes/data');
 
 module.exports = function(app) {
     app.use(express.json());
+    app.use(cors());
 
     app.use('/', indexRoute);
     app.use('/data', dataRoute);
